e5390f24f5a0b7c3a8c67fbcb2791986ca4a51d9
fernandoscjunior <fernandojrsud@icloud.com>
2025-11-18 17:23:22 -0300

    finishing week's 3 activity

--- routes/inventoryRoute.js
+++ routes/inventoryRoute.js
@@ -2,10 +2,11 @@
 const express = require("express")
 const router = new express.Router() 
 const invController = require("../controllers/invController")
+const utilities = require("../utilities")
 
 // Route to build inventory by classification view
-router.get("/type/:classificationId", invController.buildByClassificationId);
+router.get("/type/:classificationId", utilities.handleErrors(invController.buildByClassificationId));
 
-router.get("/detail/:inv_id", invController.buildVehicle);
+router.get("/detail/:inv_id", utilities.handleErrors(invController.buildVehicle));
 
 module.exports = router;
